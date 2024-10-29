4102d45b0413e88b97a88865e4bd05fcd34ac6c4
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2024-10-29 17:17:01 +0530

    feat: Update routes imports in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,19 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import { meetingRoutes } from './routes/meetings.js';
-import { taskRoutes } from './routes/task.router.js';
-import { dashboardRoutes } from "./routes/dashboardRoutes.js";
+
+import { meetingRoutes } from './routes/meeting.routes.js';
+import { taskRoutes } from './routes/task.routes.js';
+import { dashboardRoutes } from './routes/dashboard.routes.js';
+
 import { authMiddleware } from './auth.middleware.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-await mongoose.connect('mongodb://localhost:27017/meetingbot')
+await mongoose
+  .connect('mongodb://localhost:27017/meetingbot')
   .then((conn) => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 app.use(express.json());
 
@@ -21,7 +24,6 @@ app.get('/', (req, res) => {
 app.use('/api/meetings', authMiddleware, meetingRoutes);
 app.use('/api/tasks', authMiddleware, taskRoutes);
 app.use('/api/dashboard', authMiddleware, dashboardRoutes);
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
